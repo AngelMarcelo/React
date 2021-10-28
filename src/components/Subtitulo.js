c4b785daf401b95b10ef343f67500dda64c0a2f7
AngelMarcelo <ammarce@gmail.com>
2021-10-28 18:35:22 -0300

    se limpiaron los archivos sacando codigo innesesario, tambien se crearon archivos dentro de la carpeta components

---
+++ src/components/Subtitulo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// para crear un componente funcional uso rsc
+const Subtitulo = () => {
+    // aqui va la logica
+    return (
+        // a va el maquetado y un poquito de logica
+        <div>
+            <h2>Tareas de la comosion 17i</h2>
+        </div>
+    );
+};
+
+export default Subtitulo;
---
+++ src/components/Titulo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+// si escribo rcc se crea el cassComponent
+class Titulo extends Component {
+    render() {
+        return (
+            <h1>Lista de Tareas</h1>
+        );
+    }
+}
+
+export default Titulo;
